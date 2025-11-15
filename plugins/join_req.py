09252108ca522d7f4c2e86cb711b1c4db8969990
Mahesh8188 <ddbambhava8182@gmail.com>
2025-11-15 15:10:22 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -1,13 +1,20 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest
 from database.users_chats_db import db
-from info import ADMINS, AUTH_REQ_CHANNELS
+from info import ADMINS, AUTH_REQ_CHANNELS, AUTH_CHANNELS
 from pyrogram.filters import create
 
 def is_auth_req_channel(_, __, update):
     return update.chat.id in AUTH_REQ_CHANNELS
 
 @Client.on_chat_join_request(create(is_auth_req_channel))
+async def join_reqs(client, message: ChatJoinRequest):
+    await db.add_join_req(message.from_user.id, message.chat.id)
+
+def is_auth_channel(_, __, update):
+    return update.chat.id in AUTH_CHANNELS
+
+@Client.on_chat_join_request(create(is_auth_channel))
 async def join_reqs(client, message: ChatJoinRequest):
     await db.add_join_req(message.from_user.id, message.chat.id)
 
