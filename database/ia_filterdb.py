c7ca35be809dd1dedbc6b677d7c40ba753ba81ce
Mahesh8188 <ddbambhava8182@gmail.com>
2025-11-15 16:49:22 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -3,143 +3,127 @@ from struct import pack
 import re
 import base64
 from pyrogram.file_id import FileId
-from pymongo.errors import DuplicateKeyError
-from umongo import Instance, Document, fields
-from motor.motor_asyncio import AsyncIOMotorClient
-from marshmallow.exceptions import ValidationError
-from info import FILE_DB_URI, SEC_FILE_DB_URI, DATABASE_NAME, COLLECTION_NAME, MAX_BTN
+from pymongo import MongoClient, TEXT
+from pymongo.errors import DuplicateKeyError, OperationFailure
+from info import USE_CAPTION_FILTER, FILES_DATABASE_URL, SECOND_FILES_DATABASE_URL, DATABASE_NAME, COLLECTION_NAME, MAX_BTN
 
-client = AsyncIOMotorClient(FILE_DB_URI)
-mydb = client[DATABASE_NAME]
-col = mydb[COLLECTION_NAME]
+logger = logging.getLogger(__name__)
 
-# Second Database For File Saving
-sec_client = AsyncIOMotorClient(SEC_FILE_DB_URI)
-sec_db = sec_client[DATABASE_NAME]
-sec_col = sec_db[COLLECTION_NAME]
-instance = Instance.from_db(mydb)
+client = MongoClient(FILES_DATABASE_URL)
+db = client[DATABASE_NAME]
+collection = db[COLLECTION_NAME]
+second_collection = None  
+try:
+    collection.create_index([("file_name", TEXT)])
+except OperationFailure as e:
+    if 'quota' in str(e).lower():
+        if not SECOND_FILES_DATABASE_URL:
+            logger.error(f'your FILES_DATABASE_URL is already full, add SECOND_FILES_DATABASE_URL')
+        else:
+            logger.info('FILES_DATABASE_URL is full, now using SECOND_FILES_DATABASE_URL')
+    else:
+        logger.exception(e)
 
-@instance.register
-class Media(Document):
-    file_id = fields.StrField(attribute='_id')
-    file_ref = fields.StrField(allow_none=True)
-    file_name = fields.StrField(required=True)
-    file_size = fields.IntField(required=True)
-    mime_type = fields.StrField(allow_none=True)
-    caption = fields.StrField(allow_none=True)
-    file_type = fields.StrField(allow_none=True)
+if SECOND_FILES_DATABASE_URL:
+    second_client = MongoClient(SECOND_FILES_DATABASE_URL)
+    second_db = second_client[DATABASE_NAME]
+    second_collection = second_db[COLLECTION_NAME]
+    second_collection.create_index([("file_name", TEXT)])
 
-    class Meta:
-        indexes = ('$file_name', )
-        collection_name = COLLECTION_NAME
+def is_second_db_configured() -> bool:
+    return bool(SECOND_FILES_DATABASE_URL and 'second_collection' in globals() and second_collection is not None)
 
-async def get_files_db_size():
-    return (await mydb.command("dbstats"))['dataSize'] + (await sec_db.command("dbstats"))['dataSize']
-    
+def second_db_count_documents():
+     return second_collection.count_documents({})
+
+def db_count_documents():
+     return collection.count_documents({})
+
+def get_primary_db_storage():
+    stats = db.command("dbStats")
+    return stats.get('storageSize', 0)
+
+def get_secondary_db_storage():
+    if not is_second_db_configured():
+        return 0
+    stats = second_db.command("dbStats")
+    return stats.get('storageSize', 0)
 
 async def save_file(media):
-    """Save file in the database."""
+    file_id = unpack_new_file_id(media.file_id)
+    file_name = re.sub(r"@\w+|(_|\-|\.|\+)", " ", str(media.file_name))
+    file_caption = re.sub(r"@\w+|(_|\-|\.|\+)", " ", str(media.caption))
     
-    file_id, file_ref  = unpack_new_file_id(media.file_id)
-    file_name = clean_file_name(media.file_name)
+    document = {
+        '_id': file_id,
+        'file_name': file_name,
+        'file_size': media.file_size,
+        'caption': file_caption
+    }
+    
     try:
-        file = Media(
-            file_id=file_id,
-            file_ref=file_ref,
-            file_name=file_name,
-            file_size=media.file_size,
-            mime_type=media.mime_type,
-            caption=media.caption.html if media.caption else None,
-            file_type=media.mime_type.split('/')[0]
-        )
-        
-    except ValidationError:
-        print('Error occurred while saving file in database')
-        return 'err'
-    else:
-        try:
-            await file.commit()
-        except DuplicateKeyError:      
-            print(f'{getattr(media, "file_name", "NO_FILE")} is already saved in database') 
-            return 'dup'
-        else:
-            print(f'{getattr(media, "file_name", "NO_FILE")} is saved to database')
-            return 'suc'
-    if is_file_already_saved(file_id, file_name):
-        return False, 0
-
-    collection_to_use = col if not MULTIPLE_DATABASE else select_collection_based_on_size(file)
-
-    try:
-        collection_to_use.insert_one(file)
-        print(f"{file_name} is successfully saved.")
-        return True, 1
+        collection.insert_one(document)
+        logger.info(f'Saved - {file_name}')
+        return 'suc'
     except DuplicateKeyError:
-        print(f"{file_name} is already saved.")
-        return False, 0
-
-def clean_file_name(file_name):
-    """Clean and format the file name."""
-    file_name = re.sub(r"(_|\-|\.|\+)", " ", str(file_name)) 
-    unwanted_chars = ['[', ']', '(', ')', '{', '}']
-    
-    for char in unwanted_chars:
-        file_name = file_name.replace(char, '')
-        
-    return ' '.join(filter(lambda x: not x.startswith('@') and not x.startswith('http') and not x.startswith('www.') and not x.startswith('t.me'), file_name.split()))
-
-def is_file_already_saved(file_id, file_name):
-    """Check if the file is already saved in either collection."""
-    found1 = {'file_name': file_name}
-    found = {'file_id': file_id}
-
-    for collection in [col, sec_col]:
-        if collection.find_one(found1) or collection.find_one(found):
-            print(f"{file_name} is already saved.")
-            return True
-            
-    return False
-
-def select_collection_based_on_size(file):
-    """Select the appropriate collection based on database size."""
-    result = db.command('dbstats')
-    
-    if result['dataSize'] > 503316480:
-        return sec_col
-    return col
+        logger.warning(f'Already Saved - {file_name}')
+        return 'dup'
+    except OperationFailure:
+        if SECOND_FILES_DATABASE_URL:
+            try:
+                second_collection.insert_one(document)
+                logger.info(f'Saved to 2nd db - {file_name}')
+                return 'suc'
+            except DuplicateKeyError:
+                logger.warning(f'Already Saved in 2nd db - {file_name}')
+                return 'dup'
+        else:
+            logger.error(f'your FILES_DATABASE_URL is already full, add SECOND_FILES_DATABASE_URL')
+            return 'err'
 
 async def get_search_results(query, max_results=MAX_BTN, offset=0, lang=None):
-    query = query.strip()
+    query = str(query).strip()
     if not query:
         raw_pattern = '.'
     elif ' ' not in query:
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
-        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]') 
+        raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+    
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
     except:
         regex = query
-    filter = {'file_name': regex}
-    cursor = Media.find(filter)
-    cursor.sort('$natural', -1)
+
+    if USE_CAPTION_FILTER:
+        filter = {'$or': [{'file_name': regex}, {'caption': regex}]}
+    else:
+        filter = {'file_name': regex}
+
+    cursor = collection.find(filter)
+    results = [doc for doc in cursor]
+
+    if SECOND_FILES_DATABASE_URL:
+        cursor2 = second_collection.find(filter)
+        results.extend([doc for doc in cursor2])
+
     if lang:
-        lang_files = [file async for file in cursor if lang in file.file_name.lower()]
-        files = lang_files[offset:][:max_results]
+        lang_files = [file for file in results if lang in file['file_name'].lower()]
         total_results = len(lang_files)
+        files = lang_files[offset:offset + max_results]
         next_offset = offset + max_results
         if next_offset >= total_results:
             next_offset = ''
         return files, next_offset, total_results
-    cursor.skip(offset).limit(max_results)
-    files = await cursor.to_list(length=max_results)
-    total_results = await Media.count_documents(filter)
+
+    total_results = len(results)
+    files = results[offset:][:max_results]
     next_offset = offset + max_results
     if next_offset >= total_results:
-        next_offset = ''       
+        next_offset = ''   
     return files, next_offset, total_results
-    
-async def get_bad_files(query, file_type=None, offset=0, filter=False):
+
+async def delete_files(query):
     query = query.strip()
     if not query:
         raw_pattern = '.'
@@ -147,24 +131,31 @@ async def get_bad_files(query, file_type=None, offset=0, filter=False):
         raw_pattern = r'(\b|[\.\+\-_])' + query + r'(\b|[\.\+\-_])'
     else:
         raw_pattern = query.replace(' ', r'.*[\s\.\+\-_]')
+    
     try:
         regex = re.compile(raw_pattern, flags=re.IGNORECASE)
     except:
-        return []
+        regex = query
+        
     filter = {'file_name': regex}
-    if file_type:
-        filter['file_type'] = file_type
-    total_results = await Media.count_documents(filter)
-    cursor = Media.find(filter)
-    cursor.sort('$natural', -1)
-    files = await cursor.to_list(length=total_results)
-    return files, total_results
+    
+    result1 = collection.delete_many(filter)
+    
+    result2 = None
+    if SECOND_FILES_DATABASE_URL:
+        result2 = second_collection.delete_many(filter)
+    
+    total_deleted = result1.deleted_count
+    if result2:
+        total_deleted += result2.deleted_count
+    
+    return total_deleted
 
 async def get_file_details(query):
-    filter = {'file_id': query}
-    cursor = Media.find(filter)
-    filedetails = await cursor.to_list(length=1)
-    return filedetails
+    file_details = collection.find_one({'_id': query})
+    if not file_details and SECOND_FILES_DATABASE_URL:
+        file_details = second_collection.find_one({'_id': query})
+    return file_details
 
 def encode_file_id(s: bytes) -> str:
     r = b""
@@ -179,11 +170,7 @@ def encode_file_id(s: bytes) -> str:
             r += bytes([i])
     return base64.urlsafe_b64encode(r).decode().rstrip("=")
 
-def encode_file_ref(file_ref: bytes) -> str:
-    return base64.urlsafe_b64encode(file_ref).decode().rstrip("=")
-
 def unpack_new_file_id(new_file_id):
-    """Return file_id, file_ref"""
     decoded = FileId.decode(new_file_id)
     file_id = encode_file_id(
         pack(
@@ -194,6 +181,4 @@ def unpack_new_file_id(new_file_id):
             decoded.access_hash
         )
     )
-    file_ref = encode_file_ref(decoded.file_reference)
-    return file_id, file_ref
-    
+    return file_id
