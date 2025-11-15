b4b6e400766117d464f4ee69be02a2ded2ebdf38
Mahesh8188 <ddbambhava8182@gmail.com>
2025-11-15 19:03:19 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -70,19 +70,13 @@ BOT WORKING PROPERLY 🔥"""
 Bʏ @akimax06</b>"""
         
     
-    STATUS_TXT = """<b><u>🗃ᴜsᴇʀs ᴅᴀᴛᴀʙᴀsᴇ 🗃</u>
+    STATUS_TXT = """<b><u>🗃 ᴅᴀᴛᴀʙᴀsᴇ 1 🗃</u>
 
 » ᴛᴏᴛᴀʟ ᴜsᴇʀs - <code>{}</code>
 » ᴛᴏᴛᴀʟ ɢʀᴏᴜᴘs - <code>{}</code>
-» ᴘʀᴇᴍɪᴜᴍ ᴜsᴇʀs - <code>{}</code>
 » ᴜsᴇᴅ sᴛᴏʀᴀɢᴇ - <code>{} / {}</code>
 
-<u>📤 ꜰɪʟᴇs ᴅᴀᴛᴀʙᴀsᴇ 𝟷 📤</u>
-
-» ᴛᴏᴛᴀʟ ꜰɪʟᴇs - <code>{}</code>
-» ᴜsᴇᴅ sᴛᴏʀᴀɢᴇ - <code>{} / {}</code>
-
-<u>📥 ꜰɪʟᴇs ᴅᴀᴛᴀʙᴀsᴇ 𝟸 📥</u>
+<u>🗳 ᴅᴀᴛᴀʙᴀsᴇ 2 🗳</u></b>
 
 » ᴛᴏᴛᴀʟ ꜰɪʟᴇs - <code>{}</code>
 » ᴜsᴇᴅ sᴛᴏʀᴀɢᴇ - <code>{} / {}</code>
@@ -92,6 +86,7 @@ Bʏ @akimax06</b>"""
 » ᴜᴘᴛɪᴍᴇ - <code>{}</code>
 » ʀᴀᴍ - <code>{}%</code>
 » ᴄᴘᴜ - <code>{}%</code></b>"""
+    
     NEW_USER_TXT = """<b>#New_User {}
 
 ≈ ɪᴅ:- <code>{}</code>
@@ -392,6 +387,7 @@ developer : Mr.Mahesh
 ʙʏ /details ᴄᴏᴍᴍᴀɴᴅ
 </blockquote>
 Iғ ʏᴏᴜ ʜᴀᴠᴇ ᴀɴʏ ᴅᴏᴜʙᴛs ᴘʟᴇᴀsᴇ <a href="https://t.me/Mahesh8182">ᴄᴏɴᴛᴀᴄᴛ</a> ᴍʏ <a href="https://t.me/Mahesh8182">ᴀᴅᴍɪɴ</a></b>"""
+
 
 
 
